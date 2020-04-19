f79da97b4b18c6735e726755ca95fa9455293186
preischadt <thiagop@usp.br>
2020-04-19 17:31:10 -0300

    changed msgs

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -20,7 +20,7 @@ class Fun(commands.Cog):
             for _ in range(numImages):
                 async with session.get(url) as resp:
                     if resp.status != SUCESS:
-                        await ctx.send('Could not generate image...')
+                        await ctx.send('Não foi possível gerar a imagem.')
                         return
 
                     async with session.get(await resp.text()) as img:
@@ -31,7 +31,7 @@ class Fun(commands.Cog):
     async def motivar(self, ctx, numImages=1):
         """Sends a motivational picture"""
         if numImages > 3:
-            await ctx.send('OOOOOOOOOOUUOOUOU PAROU PAROU TA MUITO GRANDE ESSE NUMERO AI AMIGAO')
+            await ctx.send('Po esse numero aí ta muito grande.')
             return
 
         await self.randomImg(ctx, cfg.URLs['INSPIRO'], numImages)
@@ -46,21 +46,17 @@ class Fun(commands.Cog):
 
         await ctx.send(f'Pong! {(now-lastMsg).total_seconds() * 1000.0} ms')
 
-    @tasks.loop(seconds=1)
-    async def spamUser(self, ctx, member, cnt):
-        await member.send(f'Ou {member} bora lol pls')
-        self.spamCount += 1
-
     @commands.command()
     async def boralol(self, ctx, members: commands.Greedy[Member], *, k=cfg.MAX_BORALOL):
         """Vem pra flex manito"""
 
         if k > cfg.MAX_BORALOL:
-            await ctx.send(f'PO MANO AI O SPAM TA DEMAIS NÉ')
+            await ctx.send(f'Ai o spam ta muito grande né manito, quer spammar assim vai pro caralho')
 
         for member in members:
             for _ in range(k):
                 await member.send(f'Ou {member} bora lol pls')
+                await ctx.send(f'{member} foi chamado pro Lolzinho com sucesso')
 
 def setup(bot):
     bot.add_cog(Fun(bot))
