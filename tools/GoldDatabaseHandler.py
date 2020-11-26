e37cc7a992613c532fc57488db84b26e3442b18c
tpreischadt <thiagop@usp.br>
2020-11-26 02:12:40 +0000

    fixed database creation

--- tools/GoldDatabaseHandler.py
+++ tools/GoldDatabaseHandler.py
@@ -1,18 +1,23 @@
 import aiosqlite
+import asyncio
 import os.path
 
 class GoldDatabaseHandler:
     def __init__(self, name):
         self.database_name = name
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.startup())
 
-        if not os.path.isfile(name):
-            with open(name, 'w'): pass
-            async with aiosqlite.connect(name) as db:
+    async def startup(self):
+        if not os.path.isfile(self.database_name):
+            with open(self.database_name, 'w'): pass
+            async with aiosqlite.connect(self.database_name) as db:
                 await db.execute("""CREATE TABLE Gold (
                         original_id INTEGER NOT NULL,
                         embed_id INTEGER NOT NULL,
                         CONSTRAINT Gold_PK PRIMARY KEY (original_id));""")
                 await db.commit()
+
 
     async def lookup(self, msg_id):
         async with aiosqlite.connect(self.database_name) as db:
